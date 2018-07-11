62c32e61ea73a0a04983586125d465a6bb6b71f0
dichengyan <dichengyan@goldmantis.com>
2018-07-11 16:49:28 +0800

    添加redux devtools插件

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,9 +1,12 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers';
 
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 const StoreConfig = () => {
     return createStore(
-        rootReducer
+        rootReducer,
+        composeWithDevTools()
     );
 }
 
