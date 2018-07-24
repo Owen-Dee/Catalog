904d44739daba93786a0418f4b7b3e05a2b26a9e
dichengyan <dichengyan@goldmantis.com>
2018-07-24 17:41:52 +0800

    add header search UI

--- src/components/catalog/catalogcontent/catalogmodel/catalogmodel.tsx
+++ src/components/catalog/catalogcontent/catalogmodel/catalogmodel.tsx
@@ -46,6 +46,66 @@ export default class CatalogModel extends React.Component<any, ICatalogModelStat
         const list = this.getList();
         return (
             <div className="catalog-model">
+                <div className="header-search">
+                    <div className="search-control">
+                        <input className="search-input" placeholder="名称" />
+                        <img src={require('./img/search.svg')} alt="" />
+                    </div>
+                    <div className="select-conditions">
+                        <div className="condition-item">
+                            <div className="name-control">
+                                <div className="name">4.0套餐</div>
+                                <span className="dot-bottom"></span>
+                                <span className="dot-top"></span>
+                            </div>
+                            <div className="items">
+                                <PerfectScrollbar>
+                                    <div className="item">5.0套餐</div>
+                                    <div className="item">4.0套餐</div>
+                                    <div className="item">3.1套餐</div>
+                                    <div className="item">3.0套餐</div>
+                                </PerfectScrollbar>
+                            </div>
+                        </div>
+
+                        <div className="condition-item">
+                            <div className="name-control">
+                                <div className="name">瓷砖</div>
+                                <span className="dot-bottom"></span>
+                                <span className="dot-top"></span>
+                            </div>
+                            <div className="items">
+                                <PerfectScrollbar>
+                                    <div className="item">瓷砖</div>
+                                    <div className="item">入户门套</div>
+                                    <div className="item">筒灯</div>
+                                    <div className="item">地板</div>
+                                </PerfectScrollbar>
+                            </div>
+                        </div>
+
+                        <div className="condition-item">
+                            <div className="name-control">
+                                <div className="name">波打线</div>
+                                <span className="dot-bottom"></span>
+                                <span className="dot-top"></span>
+                            </div>
+                            <div className="items">
+                                <PerfectScrollbar>
+                                    <div className="item">波打线</div>
+                                    <div className="item">过门石</div>
+                                    <div className="item">烟道</div>
+                                    <div className="item">石坎</div>
+                                </PerfectScrollbar>
+                            </div>
+                        </div>
+
+                        <div className="expand-conditions">
+                            <img src={require('./img/expand.svg')} className="normal" alt=""/>
+                            <img src={require('./img/expand-light.svg')} className="light" alt="" />
+                        </div>
+                    </div>
+                </div>
                 <PerfectScrollbar>
                     {list}
                 </PerfectScrollbar>
--- src/entity/storeentity.tsx
+++ src/entity/storeentity.tsx
@@ -1,3 +1,8 @@
+/**
+ * @description: catalog对应的接口数据
+ * @catalogType: catalog类型
+ * @modelsData: 模型数据集合
+ */
 export interface Catalog {
 	catalogType: string,
 	modelsData: Array<any>
