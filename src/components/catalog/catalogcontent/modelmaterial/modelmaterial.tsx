b3b9338629038cf8290809eb18caa382096569c6
dichengyan <dichengyan@goldmantis.com>
2018-07-31 20:39:17 +0800

    添加img onerror

--- src/components/catalog/catalogcontent/modelmaterial/modelmaterial.tsx
+++ src/components/catalog/catalogcontent/modelmaterial/modelmaterial.tsx
@@ -14,6 +14,30 @@ export default class CatalogModel extends React.Component<ICatalogModelProps, an
         super(props);
     }
 
+    handleImageErrored(e) {
+        e.target.src = require('./img/error_img.svg');
+    }
+
+    handleModels() {
+        let models = [...this.props.models];
+        models.forEach((item) => {
+            if (item.imagesResize && item.imagesResize instanceof Array) {
+                item.imageUrl = item.imagesResize[0] ? item.imagesResize[0] : '';
+            } else {
+                item.imageUrl = '';
+            }
+            // 色板的情况下,后台不返回图片
+            if (item.hasOwnProperty('model') && item.model.color) {
+                item.background = item.model.color;
+                item.imageUrl = require('./img/transparent.svg');
+            } else {
+                item.background = '';
+            }
+        });
+
+        return models;
+    }
+
     getModelList() {
         let modelList;
         if (!this.props.models) {
@@ -23,10 +47,12 @@ export default class CatalogModel extends React.Component<ICatalogModelProps, an
                             <span>抱歉,查询数据为空!</span>
                         </div>
         } else {
-            modelList = this.props.models.map(model => {
+            let models = this.handleModels();
+            modelList = models.map(model => {
                 return <div className="model" key={model.id}>
-                    <div className="model-img">
-                        <img src={model.imagesResize[0]} />
+                    <div className="model-img" style={{ background: model.background }}>
+                        <img src={model.imageUrl}
+                            onError={this.handleImageErrored.bind(this)} />
                         <div className="favorite-controls">
                             <img className="add" src={require('./img/favorite_normal.svg')} title="收藏" />
                             <img className="added" src={require('./img/favorite_light.svg')} title="取消收藏" />
